b806b9f91666ba1d10394a503903cb1a6a701ba9
efrainespinosa <efrain.espinosa@leadsales.io>
2026-02-24 16:37:34 -0600

    feat: pluralize name currency

--- package/currency/usd.go
+++ package/currency/usd.go
@@ -25,7 +25,18 @@ func (u *USD) Denominations() []domain.Denomination {
 func (u *USD) FormatChange(items []domain.ChangeItem) string {
 	parts := make([]string, 0)
 	for _, item := range items {
-		parts = append(parts, fmt.Sprintf("%d %s", item.Count, item.Denomination.Name))
+		name := pluralize(item.Denomination.Name, item.Count)
+		parts = append(parts, fmt.Sprintf("%d %s", item.Count, name))
 	}
 	return strings.Join(parts, ", ")
+}
+
+func pluralize(name string, count int) string {
+	if count == 1 {
+		return name
+	}
+	if strings.HasSuffix(name, "nny") {
+		return name[:len(name)-1] + "ies"
+	}
+	return name + "s"
 }
